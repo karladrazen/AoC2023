91be3d5c08001b18a7196dd2ba747cbb1260066b
Karla <karla.drazen@gmail.com>
2025-05-23 23:58:41 +0200

    Test file path changed (Day3).

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,6 +1,6 @@
 import re
 
-input=open('C:/Users/Karla/Documents/Advent of Code 2023/Day3/test_input.txt','r').read()
+input=open('C:/Users/Karla/Documents/AoC2023/Day3/test_input.txt','r').read()
 lines=input.splitlines()
 
 part_number=list()
